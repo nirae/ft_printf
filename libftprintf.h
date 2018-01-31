1afe7114f64383b2ed46b368efec3fc0dfabdfbb
Nico <ndubouilh@gmail.com>
2018-01-31 13:54:45 +0100

    before the merge

--- libftprintf.h
+++ libftprintf.h
@@ -6,11 +6,7 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/11 15:08:59 by ndubouil          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2018/01/22 00:16:11 by ndubouil         ###   ########.fr       */
-=======
-/*   Updated: 2018/01/24 14:53:28 by ndubouil         ###   ########.fr       */
->>>>>>> 6d65c02773ae7a694a3414f79ea2db07883708fe
+/*   Updated: 2018/01/29 11:06:55 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +16,6 @@
 # include "./libft/libft.h"
 # include <stdarg.h>
 
-<<<<<<< HEAD
-
-=======
 typedef union		u_types
 {
 	char			c;
@@ -36,7 +29,6 @@ typedef struct		s_env
 	t_types			types;
 	int				len;
 }					t_env;
->>>>>>> 6d65c02773ae7a694a3414f79ea2db07883708fe
 
 /*
 ** ft_printf
